92ac2eb998328b63001b979f82afcec6b511acda
Ammyadav15 <ammyadav15@gmail.com>
2021-09-24 21:12:29 +0530

    Added Practice Problem Program

---
+++ batch373/Helloworld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+       while(true){
+        System.out.println( "Hello to Terminal Commands!" );
+        try {
+           Thread.sleep(60000);
+         } catch (Exception ex) {}
+
+       }
+
+    }
+
+}
